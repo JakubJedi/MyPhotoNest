e49b851ea7fe50f58465c65da168864b96f6dd75
JakubJedi <jakub.jedrszczak@gmail.com>
2025-01-07 20:02:46 +0100

    Add initial Spring Boot project structure

---
+++ src/main/java/com/jakubjedi/myphotonest/MyPhotoNestApplication.java
@@ -0,0 +1,13 @@
+package com.jakubjedi.myphotonest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhotoNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhotoNestApplication.class, args);
+	}
+
+}
